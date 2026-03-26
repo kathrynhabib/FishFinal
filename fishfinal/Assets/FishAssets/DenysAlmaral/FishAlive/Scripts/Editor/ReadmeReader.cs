4ea873e5773e3a91b360a16a3238ac70c1a7855e
Annabelle Chen <annabllechen@gmail.com>
2026-03-26 17:43:20 +0100

    Trying to solve merge conflict

--- fishfinal/Assets/FishAssets/DenysAlmaral/FishAlive/Scripts/Editor/ReadmeReader.cs
+++ fishfinal/Assets/FishAssets/DenysAlmaral/FishAlive/Scripts/Editor/ReadmeReader.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using UnityEngine;
 using UnityEditor;
 
@@ -10,4 +11,18 @@ namespace DenysAlmaral
         public TextAsset sourceText;
         public SceneAsset sceneDemo;
     }
+=======
+using UnityEngine;
+using UnityEditor;
+
+
+namespace DenysAlmaral
+{
+    //[CreateAssetMenu(fileName = "Documentation", menuName = "ReadmeDocs")]
+    public class ReadmeReader : ScriptableObject
+    {
+        public TextAsset sourceText;
+        public SceneAsset sceneDemo;
+    }
+>>>>>>> c5529c8c74fa2969ced4adda37a59680c9627969
 }
--- fishfinal/Assets/FishAssets/DenysAlmaral/FishAlive/Scripts/FishConstant.cs
+++ fishfinal/Assets/FishAssets/DenysAlmaral/FishAlive/Scripts/FishConstant.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using UnityEngine;
 
@@ -13,4 +14,21 @@ namespace FishAlive
         public const int AvoidanceMaxTries = 5;
         public const float AvoidanceTimerInterval = 0.2f;
     }
+=======
+﻿using System;
+using UnityEngine;
+
+namespace FishAlive
+{
+    public static class FishConstant
+    {
+        public const int LAYER_INTERACT = 1 << 20;
+        public const int LAYER_OBSTACLE = 1; //unity default layer as obstacles
+        public static readonly Vector3 DefaultMouthPosition = new Vector3(0f, -0.01f, 0.06f); //This will be used if mouth bone is not found
+        public const float FacingCosAng = 0.5f;
+        public const float AvoidanceAngleStep = 20f;
+        public const int AvoidanceMaxTries = 5;
+        public const float AvoidanceTimerInterval = 0.2f;
+    }
+>>>>>>> c5529c8c74fa2969ced4adda37a59680c9627969
 }
