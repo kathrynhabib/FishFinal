115af83e3456d938c79630f6c92e2401027d61b7
Kathryn <khabib@vassar.edu>
2026-03-29 19:17:53 +0200

    created encyclopedia canvas and scripts

---
+++ fishfinal/Assets/Scripts/EncyclopediaManagerScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EncyclopediaManagerScript : MonoBehaviour
+{
+    [SerializeField] public GameObject EncyclopediaEntry;
+    [SerializeField] public Transform Content;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void printEntries()
+    {
+        foreach(Transform entry in Content )
+        {
+            Destroy(entry.gameObject);
+        }
+
+        var discoveredFish = FishDiscoveryManager.Instance.GetDiscoveredFish();
+
+        foreach (fishData fish in discoveredFish)
+        {
+            GameObject entry = Instantiate(EncyclopediaEntry, Content);
+            entry.GetComponent<EncyclopediaEntry>().Setup(fish);
+        }
+    }
+}
