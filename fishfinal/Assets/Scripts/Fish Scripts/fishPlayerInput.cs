ab743c594e5bc72f9fd93f14dd76532d15013b91
Annabelle Chen <annabllechen@gmail.com>
2026-04-01 15:07:25 +0200

    made camera closer, rotate fish with camera movements

--- fishfinal/Assets/Scripts/Fish Scripts/fishPlayerInput.cs
+++ fishfinal/Assets/Scripts/Fish Scripts/fishPlayerInput.cs
@@ -5,14 +5,25 @@ public class fishPlayerInput : MonoBehaviour
 {
 
     public Transform fishCamera;
-    private fishMovement movement;
+
+    public float acceleration = 10;
+    public float turnSpeed = 3f; // turning speed of the fish
+
+    private Rigidbody rb;
 
     void Awake()
     {
-        movement = GetComponent<fishMovement>();
+        rb = GetComponent<Rigidbody>();
+
     }
 
     void Update()
+    {
+        handleMovement();
+        handleRotation();
+    
+    }
+    void handleMovement()
     {
         float moveLeftRight = Input.GetAxis("Horizontal");
         float moveForwardBack = Input.GetAxis("Vertical");
@@ -27,6 +38,26 @@ public class fishPlayerInput : MonoBehaviour
 
 
         Vector3 move = fishCamera.right * moveLeftRight + fishCamera.forward * moveForwardBack + fishCamera.up * moveUpDown;
-        movement.applyMovement(move);
+        if (move.magnitude > 0.01f)
+        {
+            Vector3 accel = move * Time.deltaTime * acceleration;
+            rb.linearVelocity += accel;
+        }
+        else
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+    }
+    void handleRotation()
+    {
+        float turnSpeed = 3f;
+
+        Vector3 lookDir = fishCamera.forward;
+
+        if (lookDir.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lookDir, Vector3.up);
+            rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        }
     }
 }
