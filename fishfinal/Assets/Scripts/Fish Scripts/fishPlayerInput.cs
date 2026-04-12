91cefa07803b889a24715e1169ecbc044fd1fb44
Annabelle Chen <annabllechen@gmail.com>
2026-04-12 20:27:05 +0200

    Reapply "Merge branch 'main' of https://github.com/kathrynhabib/FishFinal"
    This reverts commit e708953a6a02c05284e6286eb5aff1af7aa5d5f9.

--- fishfinal/Assets/Scripts/Fish Scripts/fishPlayerInput.cs
+++ fishfinal/Assets/Scripts/Fish Scripts/fishPlayerInput.cs
@@ -1,31 +1,25 @@
-using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class fishPlayerInput : MonoBehaviour
 {
-
+    public fishMovement movement;
     public Transform fishCamera;
-
-    public float acceleration = 10;
-    public float turnSpeed = 3f; // turning speed of the fish
-    public float slowingSpeed = .99f;
-    public float maxSpeed = 1.5f; // should vary per fish
-
-    private Rigidbody rb;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        movement = GetComponent<fishMovement>();
     }
 
     void Update()
     {
         handleMovement();
         handleRotation();
-
     }
     void handleMovement()
     {
+
         float moveLeftRight = Input.GetAxis("Horizontal");
         float moveForwardBack = Input.GetAxis("Vertical");
 
@@ -38,23 +32,15 @@ public class fishPlayerInput : MonoBehaviour
             moveUpDown -= 1f;
 
 
-        Vector3 move = fishCamera.right * moveLeftRight + fishCamera.forward * moveForwardBack + fishCamera.up * moveUpDown;
-        if (move.magnitude > 0.01f && move.magnitude < maxSpeed)
-        {
-            Vector3 accel = move * Time.deltaTime * acceleration;
-            rb.linearVelocity += accel;
-        }
-        else
-        {
-            rb.linearVelocity *= slowingSpeed;
-        }
+        Vector3 inputDir = fishCamera.right * moveLeftRight + fishCamera.forward * moveForwardBack + fishCamera.up * moveUpDown;
+
+        movement.applyMovement(inputDir);
+
     }
     void handleRotation() 
     {
-
         Quaternion targetRotation = Quaternion.LookRotation(fishCamera.forward, Vector3.up);
-        rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, turnSpeed * Time.deltaTime);
-
+        movement.applyRotation(targetRotation);
     }
 
 }
