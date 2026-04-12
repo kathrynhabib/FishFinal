e708953a6a02c05284e6286eb5aff1af7aa5d5f9
Annabelle Chen <annabllechen@gmail.com>
2026-04-12 17:22:58 +0200

    Revert "Merge branch 'main' of https://github.com/kathrynhabib/FishFinal"
    This reverts commit 1a20618ac09fc23aea209709c43702a4a32dcbee, reversing
    changes made to 0b4817c73d02480dce6c7019bb98d060f1b90975.

--- fishfinal/Assets/Scripts/Fish Scripts/fishDataController.cs
+++ fishfinal/Assets/Scripts/Fish Scripts/fishDataController.cs
@@ -1,6 +1,5 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class fishDataController : MonoBehaviour // contains the data bound to each fish and helps activate/ deactivate it as a player
 {
@@ -8,30 +7,20 @@ public class fishDataController : MonoBehaviour // contains the data bound to ea
     public bool isPlayer;
     public Camera mainCamera;
 
-    //private fishBehavior aiBehavior; // ai behavior
+    private fishBehavior aiBehavior; // ai behavior
     private fishPlayerInput playerInput;
-    private fishMovement movement;
-
-    // for adding confirmation for switching
-    private fishDataController selectedFish;
-    private Outline outline;
 
     void Start()
     {
-        playerInput = GetComponent<fishPlayerInput>(); // restructure these to take their data from fishdata too?
-        //aiBehavior = GetComponent<fishBehavior>();
-        movement = GetComponent<fishMovement>();
-        outline = GetComponent<Outline>();
-        outline.enabled = false;
-
-        applyFishData();
+        playerInput = GetComponent<fishPlayerInput>();
+        aiBehavior = GetComponent<fishBehavior>();
         updateControlState();
 
         Debug.Log("fishDataController Start on: " + gameObject.name + " | isPlayer: " + isPlayer);
         Debug.Log("FishDiscoveryManager.Instance is: " + FishDiscoveryManager.Instance); //will delete
 
-        //if (isPlayer)
-        //    FishDiscoveryManager.Instance.Discover(FishData);
+        if (isPlayer)
+            FishDiscoveryManager.Instance.Discover(FishData);
     }
 
     // Update is called once per frame
@@ -39,59 +28,21 @@ public class fishDataController : MonoBehaviour // contains the data bound to ea
     {
         if (!isPlayer) return;
 
-        if (SwitchConfirmPopup.Instance != null && SwitchConfirmPopup.Instance.IsOpen) return;
-
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            trySelectFish();
+            trySwitchFish();
         }
-
     }
     void updateControlState()
     {
         playerInput.enabled = isPlayer;
 
-        //if (!isPlayer) aiBehavior.enable();
-        //else aiBehavior.disable();
+        if (!isPlayer) aiBehavior.enable();
+        else aiBehavior.disable();
     }
 
-    void applyFishData()
-    {
-        if (FishData == null)
-        {
-            Debug.LogError("attach FishData pls");
-            return;
-        }
-        // these should all be replaced w adjustments in fishMovement isntead
-        movement.acceleration = FishData.acceleration;
-        movement.turnSpeed = FishData.turnSpeed;
-        movement.maxSpeed = FishData.maxSpeed;
-        movement.slowingSpeed = FishData.slowingSpeed;
-        movement.horizontalEnabled = FishData.horizontalEnabled;
-        movement.verticalEnabled = FishData.verticalEnabled;
-
-        CapsuleCollider collider = GetComponent<CapsuleCollider>();
-        if (collider != null)
-        {
-            collider.radius = FishData.colliderRadius;
-        }
-
-        Transform modelContainer = transform.Find("ModelContainer");
-        if(modelContainer != null)
-        {
-            foreach (Transform child in modelContainer) // prevents duplicates when switching fish
-                Destroy(child.gameObject);
-            
-            Instantiate(FishData.modelPrefab, modelContainer);
-        }
-
-        //aiBehavior = GetComponent<fishBehavior>();
-        //aiBehavior.SetBehavior(FishData.behaviorType);
-    }
-
-    // for switching, player presses shift to switch into a fish theyre looking at. if the other fish is roughly in the center of the screen (raycast), select.
-    // hit left shift again to confirm switch
-    private void trySelectFish()
+    // for switching, player presses shift to switch into a fish theyre looking at. if the other fish is roughly in the center of the screen (raycast), allow switch
+    private void trySwitchFish()
     {
         Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
         RaycastHit hit;
@@ -101,70 +52,29 @@ public class fishDataController : MonoBehaviour // contains the data bound to ea
         if (Physics.SphereCast(ray, 1.5f, out hit, maxDistance))
         {
 
-            fishDataController targetFish = hit.collider.GetComponentInParent<fishDataController>();
+            fishDataController targetFish = hit.collider.GetComponent<fishDataController>();
 
             if (targetFish != null && targetFish != this && targetFish.tag == "fish")
             {
-                selectedFish = targetFish;
-                highlightSelection();
-
-                Debug.Log("selected target fish, lshift again to confirm");
-                // i think here we can also add a ui popup that temporarilty freezes the screen asking the player to confirm or exit
-                SwitchConfirmPopup.Instance.Show(targetFish.FishData.fishName,
-                    () =>
-                    {
-                        trySwitchFish(targetFish);
-                        clearSelection();
-                    },
-                    () =>
-                    {
-                        clearSelection();
-                    }
-                );
-
+                playerSwitch(targetFish);
             }
         }
     }
 
-    void trySwitchFish(fishDataController selectedFish)
+    void playerSwitch(fishDataController target)
     {
         // enable fishMotion when switching to ai behavior
         // not using fishMotion for player behavior
-        Debug.Log("playerSwitch called, switching to: " + selectedFish.gameObject.name); //will delete
+        Debug.Log("playerSwitch called, switching to: " + target.gameObject.name); //will delete
         fishCameraController camera = mainCamera.GetComponent<fishCameraController>();
-        camera.target = selectedFish.transform;
+        camera.target = target.transform;
 
         isPlayer = false;
         updateControlState();
 
-        selectedFish.isPlayer = true;
-        selectedFish.updateControlState();
-        FishDiscoveryManager.Instance.Discover(selectedFish.FishData);
-
-    }
-
-    void highlightSelection()
-    {
-        Debug.Log("highlighting selected fish");
-        if (selectedFish.outline != null)
-        {
-            selectedFish.outline.enabled = true;
-        }
-    }
-
-    void unhighlightSelection()
-    {
-        Debug.Log("unhighlighting");
-        if (selectedFish.outline != null)
-        {
-            selectedFish.outline.enabled = false;
-        }
-    }
-
-    void clearSelection()
-    {
-        unhighlightSelection();
-        selectedFish = null;
+        target.isPlayer = true;
+        target.updateControlState();
+        FishDiscoveryManager.Instance.Discover(target.FishData); 
 
     }
 
--- fishfinal/Assets/Scripts/Fish Scripts/fishMovement.cs
+++ fishfinal/Assets/Scripts/Fish Scripts/fishMovement.cs
@@ -3,54 +3,26 @@ using UnityEngine;
 public class fishMovement : MonoBehaviour
 {
 
-    // restrictions/ adaptations to movement would be passed in thru fishData
-    // both ai behavior  and player input scripts would change positioning by calling applyMovement
-
     public float acceleration = 10;
-    public float turnSpeed = 3f; // turning speed of the fish
-    public float slowingSpeed = .99f;
-    public float maxSpeed = 1.5f; // should vary per fish
-
-    public bool horizontalEnabled;
-    public bool verticalEnabled;
 
     private Rigidbody rb;
-    private Vector3 velocity;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public void applyMovement(Vector3 inputDir)
+    public void applyMovement(Vector3 direction)
     {
-        Vector3 localInput = transform.InverseTransformDirection(inputDir);
-
-        if (!horizontalEnabled)
-            localInput.x = 0f;
-
-        if (!verticalEnabled)
-            localInput.y = 0f;
-
-        inputDir = transform.TransformDirection(localInput);
-
-        if (inputDir.magnitude > 0.01f)
+        if (direction.magnitude > 0.01f)
         {
-            inputDir.Normalize();
-
-            velocity += inputDir * acceleration * Time.deltaTime;
-            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            Vector3 accel = direction * Time.deltaTime * acceleration;
+            rb.linearVelocity += accel;
         }
-        else // smoothing slowing
+        else
         {
-            velocity *= slowingSpeed;
+            rb.linearVelocity = Vector3.zero;
         }
-        rb.MovePosition(rb.position + velocity * Time.deltaTime);
-
-    }
-
-    public void applyRotation(Quaternion targetRotation)
-    {
-        rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        
     }
 }
